ef56d01302790ab20d973c617d7e3824b8e4172c
SakibFuad <sfuad001@ucr.edu>
2021-12-09 01:05:36 -0800

    Add code completion half done

--- extension.js
+++ extension.js
@@ -33,10 +33,10 @@ function activate(context) {
 			//console.log(token.toString());
 
 			const range = document.getWordRangeAtPosition(position);
-            const word = document.getText(range);
+			const word = document.getText(range);
 			//console.log(word);
 			//console.log(cudaFuncs[word]);
-			try{
+			try {
 				if (word === cudaFuncs[word].id) {
 					console.log("Found: " + word);
 					return new vscode.Hover(cudaFuncs[word].value);
@@ -44,14 +44,99 @@ function activate(context) {
 					//console.log("Sorry bro! Found instead: ", word);
 				}
 			}
-			catch(err){
+			catch (err) {
 
 			}
 		}
 	});
 
+	let disposableDef = vscode.languages.registerDefinitionProvider('cuda', {
+		provideDefinition(document, position, token) {
+			console.log("Hurray! Definition is working");
+			//console.log(position.toString());
+			//console.log(token.toString());
+
+			const range = document.getWordRangeAtPosition(position);
+			const word = document.getText(range);
+			console.log(word);
+			let pos2 = new vscode.Position(20, 17);
+			//console.log(cudaFuncs[word]);
+			return new vscode.Location(document.uri, pos2);
+		}
+	});
+
+
+	const provider1 = vscode.languages.registerCompletionItemProvider('cuda', {
+
+		provideCompletionItems(document, position, token, context) {
+
+			// a simple completion item which inserts `Hello World!`
+			const simpleCompletion = new vscode.CompletionItem('Hello World!');
+			const simpleCompletion2 = new vscode.CompletionItem('Hello World!2');
+
+			// a completion item that inserts its text as snippet,
+			// the `insertText`-property is a `SnippetString` which will be
+			// honored by the editor.
+			const snippetCompletion = new vscode.CompletionItem('Good part of the day');
+			snippetCompletion.insertText = new vscode.SnippetString('Good ${1|morning,afternoon,evening|}. It is ${1}, right?');
+			snippetCompletion.documentation = new vscode.MarkdownString("Inserts a snippet that lets you select the _appropriate_ part of the day for your greeting.");
+
+			// a completion item that can be accepted by a commit character,
+			// the `commitCharacters`-property is set which means that the completion will
+			// be inserted and then the character will be typed.
+			const commitCharacterCompletion = new vscode.CompletionItem('console');
+			commitCharacterCompletion.commitCharacters = ['.'];
+			commitCharacterCompletion.documentation = new vscode.MarkdownString('Press `.` to get `console.`');
+
+			// a completion item that retriggers IntelliSense when being accepted,
+			// the `command`-property is set which the editor will execute after 
+			// completion has been inserted. Also, the `insertText` is set so that 
+			// a space is inserted after `new`
+			const commandCompletion = new vscode.CompletionItem('new');
+			commandCompletion.kind = vscode.CompletionItemKind.Keyword;
+			commandCompletion.insertText = 'new ';
+			commandCompletion.command = { command: 'editor.action.triggerSuggest', title: 'Re-trigger completions...' };
+
+			// return all completion items as array
+			return [
+				simpleCompletion,
+				simpleCompletion2,
+				snippetCompletion,
+				commitCharacterCompletion,
+				commandCompletion
+			];
+		}
+	});
+
+
+	const providerCompletion = vscode.languages.registerCompletionItemProvider(
+		'cuda',
+		{
+			provideCompletionItems(document, position) {
+
+				// get all text until the `position` and check if it reads `console.`
+				// and if so then complete if `log`, `warn`, and `error`
+				const linePrefix = document.lineAt(position).text.substr(0, position.character);
+				console.log(position.character);
+				if (!linePrefix.endsWith('cudaMemc')) {
+					return undefined;
+				}
+
+				return [
+					new vscode.CompletionItem('log', vscode.CompletionItemKind.Method),
+					new vscode.CompletionItem('warn', vscode.CompletionItemKind.Method),
+					new vscode.CompletionItem('error', vscode.CompletionItemKind.Method),
+				];
+			}
+		},
+		'c' // triggered whenever a '.' is being typed
+	);
+
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(disposableLang);
+	context.subscriptions.push(disposableDef);
+	context.subscriptions.push(provider1);
+	context.subscriptions.push(providerCompletion);
 }
 
 // this method is called when your extension is deactivated
